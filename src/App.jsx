dee2b9bca190d77b2676119176141d9f731fa78a
Mouad-MCH <mouadcharadi021@gmail.com>
2026-02-16 18:00:32 +0000

    setup files

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className='container'>
+
+      <div className='users_content'></div>
+      <div className='posts_content'></div>
+
+    </main>
+  )
+}
+
+export default App
